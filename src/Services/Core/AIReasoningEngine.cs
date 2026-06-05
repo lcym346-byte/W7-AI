d6535c4a9d505181b8fda31c7c96da7195764e42
lcym346-byte <lcym346@gmail.com>
2026-06-06 02:30:48 +0800

    Update AIReasoningEngine.cs

--- src/Services/Core/AIReasoningEngine.cs
+++ src/Services/Core/AIReasoningEngine.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 using System.Text.RegularExpressions;
 using AIAgentTool.Models;
-using System.Data;
+
 
 namespace AIAgentTool.Services.Core
 {
