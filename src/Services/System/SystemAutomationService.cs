e8441b8aad7f265fe56db9c7887040eaa1820eca
lcym346-byte <lcym346@gmail.com>
2026-06-06 02:27:08 +0800

    Update SystemAutomationService.cs

--- src/Services/System/SystemAutomationService.cs
+++ src/Services/System/SystemAutomationService.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
+using System.Collections.Specialized;
 
 namespace AIAgentTool.Services.System
 {
