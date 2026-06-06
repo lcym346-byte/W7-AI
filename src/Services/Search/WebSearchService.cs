d8bf4d1f51c6962c3035e99d4736dbeb8885463a
lcym346-byte <lcym346@gmail.com>
2026-06-07 02:27:20 +0800

    Add using directive for System.Diagnostics

--- src/Services/Search/WebSearchService.cs
+++ src/Services/Search/WebSearchService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 using System.Threading;
 using AIAgentTool.Models;
 using AIAgentTool.Utils;
